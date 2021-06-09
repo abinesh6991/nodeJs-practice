f4c75a57ce95d169f6d694475e810d36b60ee3ea
abinesh6991 <abinesh6991@gmail.com>
2021-06-09 22:07:26 +0530

    add functions

---
+++ common.js
@@ -0,0 +1,13 @@
+function greet() {
+    return `Hello world`
+};
+
+let letters = () => {
+    return `ABCD`
+};
+
+let numbers = function(){
+    return `1,2,3,4`
+};
+
+module.exports = {greet, letters, numbers};
---
+++ user.js
@@ -0,0 +1,9 @@
+let func = require("./common");
+
+let print = func.greet();
+let print2 = func.letters();
+let print3 = func.numbers();
+
+console.log(`greet : ${print}`);
+console.log(`letters : ${print2} `);
+console.log(`numbers: ${print3}`);
